279c081a9afa3a991bb4d92c120cd3b0999a0b47
Scorpion4207 <146665854+Scorpion4207@users.noreply.github.com>
2023-12-26 21:50:47 +0700

    add lesson2.76-77

---
+++ lesson2.76/main.js
@@ -0,0 +1,16 @@
+const firstName = 'Andrey'
+const lastName = 'Telitsyn'
+const birthday = '17.02.2001'
+let age = '22'
+age ='23'
+let theNameOfTheStudy = 'Aroken.ru'
+
+console.log(firstName, lastName, birthday, age, 'years', theNameOfTheStudy); 
+// const от let отличается толь тем, что в нем уже нельзя переписывать значение в переменной, иначе fail :( 
+
+let hobby = 'Hard training'
+    , dream = 'Just to be happy'
+const favorite_game = 'Mortal Kombat'
+
+
+console.log(hobby, dream, favorite_game);
